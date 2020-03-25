bb2fdb5b6fe8a402431537615041100029546fa0
Bala <zzyy9487@gmail.com>
2020-03-25 12:48:14 +0800

    layout_recyclerView_cell

--- app/src/main/java/com/example/fakebook/MsgActivity.kt
+++ app/src/main/java/com/example/fakebook/MsgActivity.kt
@@ -2,11 +2,32 @@ package com.example.fakebook
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class MsgActivity : AppCompatActivity() {
+
+    var userName: String = ""
+    var userId: String = ""
+    var userToken: String = ""
+    lateinit var apiInterface: APIInterface
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_msg)
+
+        userName = intent?.getStringExtra("name") ?: return
+        userId = intent?.getStringExtra("id") ?: return
+        userToken = intent?.getStringExtra("api_token") ?: return
+
+        val retrofit = Retrofit.Builder()
+            .baseUrl("http://34.80.201.121/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+        apiInterface = retrofit.create(APIInterface::class.java)
+
+
+
+
     }
 }
